77c6663c0ff85ecae8fcc4e48bee7921e4828cc0
CristianConcha <ciconcha@miuandes.cl>
2021-04-27 17:08:32 -0400

    correct the fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -2,13 +2,12 @@
 function fibonacci(num) {
     
     if (num < 2) {
-        return 2
+        return num
     }
     else {
         return fibonacci(num - 1) + fibonacci(num-2)
     }
 
 }
-
 
 console.log(fibonacci(5))
